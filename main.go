3e63edfba5d024be9f2ef18d0906067b606d5e43
Pancaakeman <haxionrising@gmail.com>
2025-11-16 20:17:32 +0000

    Added Simple Logging

--- main.go
+++ main.go
@@ -1,11 +1,16 @@
 /*
 Copyright © 2025 NAME HERE <EMAIL ADDRESS>
-
 */
 package main
 
-import "oreonproject/basalt/cmd"
+import (
+	"log"
+	"oreonproject/basalt/cmd"
+	"oreonproject/basalt/utils"
+)
 
 func main() {
 	cmd.Execute()
+	utils.LogSetup("logs/main.log")
+	log.Print("cute test :3")
 }
