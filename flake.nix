4af442269ab3105754b561b0efb8cdaeae45be43
Simon <175155691+Simon-Weij@users.noreply.github.com>
2025-11-16 20:05:41 +0100

    Added flake.nix

---
+++ flake.nix
@@ -0,0 +1,35 @@
+{
+  inputs = {
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs =
+    inputs@{ flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      imports = [ ];
+      systems = [ "x86_64-linux" ];
+      perSystem =
+        {
+          config,
+          self',
+          inputs',
+          pkgs,
+          system,
+          ...
+        }:
+        {
+          devShells = {
+            default = pkgs.mkShell {
+              buildInputs = [
+                pkgs.gcc
+                pkgs.go
+                pkgs.git
+              ];
+            };
+          };
+        };
+
+      flake = { };
+    };
+}
