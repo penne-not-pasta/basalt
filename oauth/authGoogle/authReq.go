0513822e6a2385ed06b72fd49966e30139cdf3c6
Pancaakeman <haxionrising@gmail.com>
2025-11-22 22:36:41 +0000

    Added Comments for Clarity

--- oauth/authGoogle/authReq.go
+++ oauth/authGoogle/authReq.go
@@ -7,8 +7,10 @@ import (
 )
 
 func CraftAuthURI() string {
-	authServer := "https://accounts.google.com/o/oauth2/v2/auth"
+	authServer := "https://accounts.google.com/o/oauth2/v2/auth" // Defines the base Auth server to send the request to
+	// Creates a Values struct to hold the Key and Value pairs for OAuth2
 	params := url.Values{}
+	// Creates a URL struct for the Redirect URL
 	redirect := &url.URL{
 		Scheme: "http",
 		Host:   "localhost:8080",
@@ -16,17 +18,17 @@ func CraftAuthURI() string {
 	}
 
 	params.Add("client_id", "844897693697-23j2de25lbf5fdmsh5lfs3hn0fr9kkh3.apps.googleusercontent.com")
-	params.Add("access_type", "offline")
+	params.Add("access_type", "offline") // Allows us to generate refresh tokens without the client going into their browsers
 	params.Add("redirect_uri", redirect.String())
 	params.Add("include_granted_scopes", "true")
-	params.Add("response_type", "code")
-	params.Add("scope", strings.Join([]string{"openid", "https://www.googleapis.com/auth/calendar", "https://www.googleapis.com/auth/drive"}, " "))
-	params.Add("code_challenge", CodeChallengeGen())
-	params.Add("code_challenge_method", "S256")
-	params.Add("state", StateTokGen())
+	params.Add("response_type", "code")                                                                                                             // Response type is an AuthCode
+	params.Add("scope", strings.Join([]string{"openid", "https://www.googleapis.com/auth/calendar", "https://www.googleapis.com/auth/drive"}, " ")) // our scopes
+	params.Add("code_challenge", CodeChallengeGen())                                                                                                // Generates a Code Challenge
+	params.Add("code_challenge_method", "S256")                                                                                                     // Code Challenge method is a base64 RAWURLENCODED SHA256 hash of the CodeVerifier
+	params.Add("state", StateTokGen())                                                                                                              // Generates a State Token to Prevent Cross Site Request Forgery
 
-	authURL := fmt.Sprintf("%s?%s", authServer, params.Encode())
-	authURL = strings.ReplaceAll(authURL, "+", "%20")
+	authURL := fmt.Sprintf("%s?%s", authServer, params.Encode()) // Formats the Parameters with the AuthURL
+	authURL = strings.ReplaceAll(authURL, "+", "%20")            // Replaces all +'s with %20 as google OAuth2 servers crash out without them???
 	fmt.Println(authURL)
 	return authURL
 
