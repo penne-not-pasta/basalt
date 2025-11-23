821768e3decb34d7635f37199758d117e4d89809
Pancaakeman <haxionrising@gmail.com>
2025-11-23 00:09:50 +0000

    Improved the google cli command

--- cmd/auth/google.go
+++ cmd/auth/google.go
@@ -2,16 +2,20 @@ package auth
 
 import (
 	"log"
+	"oreonproject/basalt/oauth/authGoogle"
 	"os/exec"
 
 	"github.com/spf13/cobra"
 )
 
 var GoogleCmd = &cobra.Command{
-	Use:   "google",
-	Short: "Login with google",
+	Use:     "google",
+	Short:   "Login with google",
+	Long:    "Description:\nOpens the Authorisation URL in your default browser,\nThere you can grant the application permissions to access your resources on google's cloud services\nBasalt asks for modification access to your Google Drive and Google Calendar to provide a seamless integration with other features",
+	Aliases: []string{"g", "ggl"},
 	Run: func(cmd *cobra.Command, args []string) {
-		url := "https://google.com"
+
+		url := authGoogle.CraftAuthURI()
 		err := exec.Command("xdg-open", url).Start()
 		if err != nil {
 			cmd.Println("Error opening browser:", err)
